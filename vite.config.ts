dfe5464a3cdec1627b42c8fd66e3c177331a0687
imurar <imurar@outlook.com>
2025-09-07 21:31:33 +0900

    refactor: add react and add base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), react()],
+  base: "/imurar-portfolio/",
 });
