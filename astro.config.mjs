6dacc715cc7fb341ecc25fc75432095063ed48d8
imurar <imurar@outlook.com>
2026-07-13 20:42:12 +0900

    [refactor] ホームページを Astro に変換

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
 import { defineConfig } from 'astro/config';
-import vue from '@astrojs/vue';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://imurar.github.io',
-  integrations: [
-    vue(),
-    tailwind()
-  ],
   output: 'static'
 });
